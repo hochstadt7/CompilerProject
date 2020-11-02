12f16a9ea615980f77bb9e3ee1ac021ddb111e8c
roee <roee@roee-Lenovo-ideapad-510S-14ISK>
2020-11-02 12:55:46 +0200

    Added visitor that finds all offspring of a class and changes the method declarations.

---
+++ Compiler1/src/ast/OffspringFinderMethodRenamerVisitor.java
@@ -0,0 +1,220 @@
+package ast;
+
+import java.util.HashSet;
+
+public class OffspringFinderMethodRenamerVisitor implements Visitor {
+	public int initial;
+	public String origMethodName, newMethodName;
+	public HashSet<String> OffspringNames;
+	
+	public OffspringFinderMethodRenamerVisitor(int _initial, String _origMethodName, String _newMethodName) {
+		initial = _initial;
+		origMethodName = _origMethodName;
+		newMethodName = _newMethodName;
+		OffspringNames = new HashSet<String>();
+	}
+
+	@Override
+	public void visit(Program program) {
+		OffspringNames.add(program.classDecls().get(initial).name());
+		for (int i = initial + 1; i < program.classDecls().size(); i++) {
+			program.classDecls().get(i).accept(this);
+		}
+	}
+
+	@Override
+	public void visit(ClassDecl classDecl) {
+		String fatherName = classDecl.superName();
+		if (fatherName == null) {
+			return;
+		}
+		if (OffspringNames.contains(fatherName)) {
+			OffspringNames.add(classDecl.name());
+			for (MethodDecl decl : classDecl.methoddecls()) {
+				decl.accept(this);
+			}
+		}
+	}
+
+	@Override
+	public void visit(MainClass mainClass) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(MethodDecl methodDecl) {
+		if (methodDecl.name().equals(origMethodName)) {
+			methodDecl.setName(newMethodName);
+		}
+	}
+
+	@Override
+	public void visit(FormalArg formalArg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(VarDecl varDecl) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(BlockStatement blockStatement) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(IfStatement ifStatement) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(WhileStatement whileStatement) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(SysoutStatement sysoutStatement) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(AssignStatement assignStatement) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(AssignArrayStatement assignArrayStatement) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(AndExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(LtExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(AddExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(SubtractExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(MultExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(ArrayAccessExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(ArrayLengthExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(MethodCallExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(IntegerLiteralExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(TrueExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(FalseExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(IdentifierExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(ThisExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(NewIntArrayExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(NewObjectExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(NotExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(IntAstType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(BoolAstType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(IntArrayAstType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(RefType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
