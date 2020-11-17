0e51ddec05b9eccdb68fcae0ba8833b836758601
Tom Turgeman <45759109+Tomatzikli@users.noreply.github.com>
2020-11-17 23:21:50 +0200

    Add files via upload
    Created VariableInstanceRenamer.

---
+++ Compiler1/src/ast/VariableInstanceRenamer.java
@@ -0,0 +1,244 @@
+package ast;
+
+
+public class VariableInstanceRenamer implements Visitor {
+	private String origVarName, newVarName;
+	private int external;
+	private boolean is_external = true;
+
+	
+	public VariableInstanceRenamer(String _origVarName, String _newVarName, int _external) {
+		origVarName = _origVarName;
+		newVarName = _newVarName;	
+		external = _external;
+	}
+
+	@Override
+	public void visit(Program program) {
+		
+	}
+
+	@Override
+	public void visit(ClassDecl classDecl) {
+		
+	}
+
+	@Override
+	public void visit(MainClass mainClass) {
+
+	}
+
+	@Override
+	public void visit(MethodDecl methodDecl) {
+		//check if the var is declared as a formal or var
+        for (FormalArg formal : methodDecl.formals()) {
+            formal.accept(this);
+        }
+        for (VarDecl varDecl : methodDecl.vardecls()) {
+            varDecl.accept(this);
+        }
+        //checkif the var was overridden from outside or not of the scope
+        if((external == 0) && is_external == false || (external == 1) && is_external == true)
+	        for (Statement stmt : methodDecl.body()) {
+	            stmt.accept(this);
+	        }
+        methodDecl.ret().accept(this);
+	}
+
+	@Override
+	public void visit(FormalArg formalArg) {
+		if(formalArg.name().equals(origVarName))//var is formal
+		{
+			is_external = false;
+			if(external == 0)
+				formalArg.setName(newVarName);
+		}
+
+	}
+
+	@Override
+	public void visit(VarDecl varDecl) {
+		if(varDecl.name().equals(origVarName))//var declared inside
+		{
+			is_external = false;
+			if(external == 0)
+				varDecl.setName(newVarName);
+		}
+
+	}
+
+	@Override
+	public void visit(BlockStatement blockStatement) {
+		for (Statement s : blockStatement.statements()) {
+			s.accept(this);
+		}
+
+	}
+
+	@Override
+	public void visit(IfStatement ifStatement) {
+		ifStatement.cond().accept(this);
+		ifStatement.thencase().accept(this);
+		ifStatement.elsecase().accept(this);
+	}
+
+	@Override
+	public void visit(WhileStatement whileStatement) {
+		whileStatement.cond().accept(this);
+		whileStatement.body().accept(this);
+
+	}
+
+	@Override
+	public void visit(SysoutStatement sysoutStatement) {
+		sysoutStatement.arg().accept(this);
+
+	}
+
+	@Override
+	public void visit(AssignStatement assignStatement) {
+		assignStatement.rv().accept(this);
+		if(assignStatement.lv() == origVarName)
+			assignStatement.setLv(newVarName);
+
+	}
+
+	@Override
+	public void visit(AssignArrayStatement assignArrayStatement) {
+		assignArrayStatement.rv().accept(this);
+		if(assignArrayStatement.lv() == origVarName)
+			assignArrayStatement.setLv(newVarName);
+	}
+
+	@Override
+	public void visit(AndExpr e) {
+		e.e1().accept(this);
+		e.e2().accept(this);
+
+	}
+
+	@Override
+	public void visit(LtExpr e) {
+		e.e1().accept(this);
+		e.e2().accept(this);
+
+	}
+
+	@Override
+	public void visit(AddExpr e) {
+		e.e1().accept(this);
+		e.e2().accept(this);
+
+	}
+
+	@Override
+	public void visit(SubtractExpr e) {
+		e.e1().accept(this);
+		e.e2().accept(this);
+
+	}
+
+	@Override
+	public void visit(MultExpr e) {
+		e.e1().accept(this);
+		e.e2().accept(this);
+
+	}
+
+	@Override
+	public void visit(ArrayAccessExpr e) {
+		e.arrayExpr().accept(this);
+		e.indexExpr().accept(this);
+
+	}
+
+	@Override
+	public void visit(ArrayLengthExpr e) {
+		e.arrayExpr().accept(this);
+
+	}
+
+	@Override
+	public void visit(MethodCallExpr e) {
+		e.ownerExpr().accept(this);
+		for(Expr arg: e.actuals())
+		{
+			arg.accept(this);
+		}
+
+	}
+
+	@Override
+	public void visit(IntegerLiteralExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(TrueExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(FalseExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(IdentifierExpr e) {
+		if(e.id() == origVarName)
+			e.setId(newVarName);
+
+	}
+
+	@Override
+	public void visit(ThisExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(NewIntArrayExpr e) {
+		e.lengthExpr().accept(this);
+
+	}
+
+	@Override
+	public void visit(NewObjectExpr e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(NotExpr e) {
+		e.e().accept(this);
+
+	}
+
+	@Override
+	public void visit(IntAstType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(BoolAstType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(IntArrayAstType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(RefType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
